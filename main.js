b974244f9e3d9574f3e657b5ad4e839018545b47
Shila Casteels <r0257725@student.thomasmore.be>
2016-12-13 13:33:04 +0100

    Code, met foutjes

--- main.js
+++ main.js
@@ -17,14 +17,33 @@ var Settings = function (url) {
 };
 
 var Drone = function (id, name, mac) {
-	this._id = id
+	this._id = id;
 	this.name = name;
 	this.mac = mac;
 };
+var FileHead = function (id, ref){
+        this.id = id;
+        this.droneref = ref;
+};
+var Files = function(id,date_loaded, date_first_rec, date_last_rec, content_id ){
+        this.id = id;
+        this.date_loaded = date_loaded;
+        this.date_first_rec = date_first_rec;
+        this.date_last_rec;
+        this.content_id = content_id;
+};
+var Contents = function(url, ref, id){
+        this.url = url;
+        this.Fileref = ref;
+        this.Droneid = id;
+}
 
 var dronesSettings = new Settings("/drones?format=json");
 
 dal.clearDrone();
+//dal.clearFileHead();
+//dal.clearFiles();
+//dal.clearContents();
 
 request(dronesSettings, function (error, response, dronesString) {
 	var drones = JSON.parse(dronesString);
@@ -36,7 +55,48 @@ request(dronesSettings, function (error, response, dronesString) {
 			var drone = JSON.parse(droneString);
 			dal.insertDrone(new Drone(drone.id, drone.name, drone.mac_address));
 		});
-	});
+        var fileheadsettings = new Settings ("/drones/" + drone.id + "/files?format=json");
+        request (fileheadsettings, function(error, response, fileheadString){
+            var FileHead = JSON.parse(fileheadString);
+            console.log(FileHead);
+            console.log("**************************************************************************");
+            FileHead.forEach(function(FileHeads){
+                  var fileheadSettings = new Settings ("/drones/" + drone.id + "/files/"+FileHead.id+"?format=json");
+                  request(fileheadSettings, function(error, response, filehString){
+                      var FileHeads = JSON.parse(filehString);
+                      dal.insertFileHeads (new FileHead(FileHead.id, FileHead.droneref));
+                  });
+            var filessettings = new Settings ("/drones/"+drone.id+"/files/"+FileHead.id+"?format=json");
+            request(filessettings, function(error, response, fileString){
+                var Files = JSON.parse(fileString);
+                console.log(Files);
+                console.log("*********************************************************************************");
+                Files.forEach(function(file){
+                    var fileSettings= new Settings("/drones/"+drone.id+"/files/"+FileHead.id+"/contents?format=json");
+                    request(fileSettings,function(error, response, fileString){
+                        var file = JSON.parse(fileString);
+                        dal.insertFiles(new Files(Files.id, Files.date_loaded, Files.date_first_rec, Files.date_last_rec,
+                        Files.content_id));
+                        });
+                    var contentssettings = new Settings ("/drones/"+drone.id+"/files/"+FileHead.id+"/contents?format=json");
+                    request(contentssettings, function(error, response, contentsString){
+                        var Contents = JSON.parse(contentsString);
+                        console.log(Contents);
+                        console.log("**********************************************************************************");
+                        Contents.forEach(function(content){
+                            var contentSettings = new Settings ("/drones/"+drone.id+"/files/"+FileHead.id+"/contents/"+Contents.url+"?format=json");
+                            request(contentSettings, function(error, response, contentString){
+                                var content = JSON.parse(contentString);
+                                dal.insertContents(new Contents(Contents.url, Contents.Fileref, Contents.Droneid));
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
 });
+
 
 console.log("Hello Shila!");
